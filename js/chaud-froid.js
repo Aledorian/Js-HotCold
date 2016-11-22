0d95f4189bb2e4e98515736d190ff7077c852a60
simplon <aledorianlegrand@gmail.com>
2016-11-22 12:19:52 +0100

    back fini

--- js/chaud-froid.js
+++ js/chaud-froid.js
@@ -1,24 +1,18 @@
-var x = Math.floor( 100 * Math.random() );
+var x = Math.ceil( 100 * Math.random() );
 
 console.log("random = " + x);
 
 function jeu(){
-  while (choix != x) {
-    var choix = prompt("Nombre en 1 et 100")
-    var result = Math.sign(x - choix)
+  var choix = document.getElementById('input').value;
+  var distance = Math.abs(x - choix);
 
-    if (result == -1) {
-      var resu = choix - x
-    }
-    else {
-      resu = x - choix
-    }
-
-    if (resu < 25) {
-  	  alert("chaud")
-    }
-    else {
-      alert("froid")
-    }
+  if (distance == 0) {
+    alert("BRAVOOOOOOO !!!!!!");
+  }
+  else if (distance < 10) {
+    document.getElementById('indice').innerHTML = "chaud";
+  }
+  else {
+    document.getElementById('indice').innerHTML = "froid";
   }
 }
